11a5318f9cff9820929f487016c15878917c79f4
Kamil Podlesak <kamil.podlesak@lmc.eu>
2016-07-10 16:51:10 +0200

    subsystem remove implemented (correctly)

--- src/main/java/eu/lmc/wildfly/haproxy/extension/SubsystemRemove.java
+++ src/main/java/eu/lmc/wildfly/haproxy/extension/SubsystemRemove.java
@@ -1,9 +1,15 @@
 package eu.lmc.wildfly.haproxy.extension;
 
+import eu.lmc.wildfly.haproxy.server.HaProxyAgentService;
 import org.jboss.as.controller.AbstractRemoveStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceName;
+
+import java.util.Set;
 
 /**
  * Handler responsible for removing the subsystem resource from the model
@@ -11,6 +17,8 @@ import org.jboss.dmr.ModelNode;
  * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
  */
 class SubsystemRemove extends AbstractRemoveStepHandler {
+
+    private final Logger logger = Logger.getLogger(SubsystemRemove.class);
 
     static final SubsystemRemove INSTANCE = new SubsystemRemove();
 
@@ -20,8 +28,17 @@ class SubsystemRemove extends AbstractRemoveStepHandler {
 
     @Override
     protected void performRuntime(OperationContext context, ModelNode operation, ModelNode model) throws OperationFailedException {
-        //TODO: Remove any services installed by the corresponding add handler here
-        //context.removeService(ServiceName.of("some", "name"));
+        logger.info("removing subsystem...");
+
+        final Resource submodel = context.getOriginalRootResource().navigate(context.getCurrentAddress());
+        String childType = SubsystemExtension.SERVER_PATH.getKey();
+        final Set<Resource.ResourceEntry> children = submodel.getChildren(childType);
+        for (Resource.ResourceEntry serverResource : children) {
+            String key = serverResource.getPathElement().getValue();
+            final ServiceName name = HaProxyAgentService.createServiceName(key);
+            logger.debug("removing: " + serverResource);
+            context.removeService(name);
+        }
     }
 
 
